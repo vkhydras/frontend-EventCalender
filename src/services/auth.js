4c675cd91dc6b5c398ce26cf83cd13f6c0226140
vkhydras <134268942+vkhydras@users.noreply.github.com>
2023-07-24 12:32:08 +0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,12 +3,12 @@
 import api from './api';
 
 const authService = {
-  login: async (userData, onLogin) => { // Pass onLogin function as a parameter
+  login: async (userData, onLogin) => {
     try {
       const response = await api.post('/users/login', userData);
       const { token } = response.data;
 
-      // Save the token in localStorage or cookies
+      // Save the token in localStorage
       localStorage.setItem('authToken', token);
 
       // Call the onLogin function to update the isAuthenticated state in the App component
@@ -16,7 +16,12 @@ const authService = {
 
       return response.data;
     } catch (error) {
-      throw error;
+      // Handle authentication error
+      if (error.response && error.response.data) {
+        throw new Error(error.response.data.message);
+      } else {
+        throw new Error('An error occurred during login.');
+      }
     }
   },
 
@@ -25,7 +30,12 @@ const authService = {
       const response = await api.post('/users/register', userData);
       return response.data;
     } catch (error) {
-      throw error;
+      // Handle registration error
+      if (error.response && error.response.data) {
+        throw new Error(error.response.data.message);
+      } else {
+        throw new Error('An error occurred during registration.');
+      }
     }
   },
 
