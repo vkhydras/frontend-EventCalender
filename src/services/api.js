c7c2cb4697eaafbc44c33cdc94d8755b7d0f7f2c
vkhydras <134268942+vkhydras@users.noreply.github.com>
2023-07-24 10:49:14 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://EventCalendar-api.orender.com',
+  baseURL: 'https://eventcalender-server.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`, // Include the token from local storage
